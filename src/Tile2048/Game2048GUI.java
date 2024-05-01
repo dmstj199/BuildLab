3d9e99df796194d2ed5d62c75b8bf25d9569af7f
eunseo <dmstj199@naver.com>
2024-05-01 19:09:58 +0900

    기본 틀 제작

--- src/Tile2048/Game2048GUI.java
+++ src/Tile2048/Game2048GUI.java
@@ -1,6 +1,7 @@
 package Tile2048;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -21,6 +22,37 @@ public class Game2048GUI extends JFrame {
                 super.keyPressed(e);
             }
         });
-//2024.04.30
+
+        JPanel boardPanel = new JPanel(){
+            @Override
+            protected void paintComponent(Graphics g){
+                super.paintComponent(g);
+                for(int i = 0; i < tiles.length; i++){
+                    for (int j = 0; j < tiles[0].length; j++){
+                        int x = j*tileSize;
+                        int y = i*tileSize;
+                        g.setColor(Color.LIGHT_GRAY);
+                        g.fillRect(x,y,tileSize, tileSize);
+
+                        String number = tiles[i][j] > 0 ? String.valueOf(tiles[i][j]) : "";
+                        g.setColor(Color.BLACK);
+                        g.drawString(number, x + tileSize / 2, y + tileSize /2);
+                    }
+                }
+            }
+        };
+        add(boardPanel); //프레임에 추가
+
+        setVisible(true);
+    }
+
+    public static void main(String[] args){
+        int[][] initialTiles = {
+                {2, 4, 8, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0},
+                {0, 0, 0, 0}
+        };
+        new Game2048GUI(initialTiles);
     }
 }
